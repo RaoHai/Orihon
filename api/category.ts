2a8e7d01e9efd7d4ce657bc63ea64442e64b94cc
raohai.rh <raohai.rh@antgroup.com>
2022-01-24 08:49:17 +0800

    feat: add category cache control

--- api/category.ts
+++ api/category.ts
@@ -1,6 +1,8 @@
 import urllib from 'urllib';
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-export default async function handler(request: VercelRequest, response: VercelResponse) {
-  return await urllib.curl('https://raw.githubusercontent.com/cbeta-org/xml-p5/master/canons.json', { dataType: 'json' });
+export default async function handler(_req: VercelRequest, res: VercelResponse) {
+  res.setHeader('Cache-Control', 's-maxage=86400');
+  const data = await urllib.curl('https://raw.githubusercontent.com/cbeta-org/xml-p5/master/canons.json', { dataType: 'json' });
+  res.status(200).json(data);
 }
