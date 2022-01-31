0698984e66895671c5e9d148b0bad2505702b6ed
陆沉 <surgesoft@gmail.com>
2022-01-31 17:08:26 +0800

    feat: add api category id

--- api/category.ts
+++ api/category.ts
@@ -1,8 +1,9 @@
 import urllib from 'urllib';
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-export default async function handler(_req: VercelRequest, res: VercelResponse) {
-  res.setHeader('Cache-Control', 's-maxage=86400');
+export default async function listCategories(_req: VercelRequest, res: VercelResponse) {
   const result = await urllib.curl('https://raw.githubusercontent.com/cbeta-org/xml-p5/master/canons.json', { dataType: 'json' });
+
+  res.setHeader('Cache-Control', 's-maxage=86400');
   res.status(200).json(result.data);
 }
--- pages/category/index.tsx
+++ pages/category/index.tsx
@@ -1,6 +1,48 @@
-export default function Menu() {
+import Link from "next/link";
 
+export interface Category {
+  title: string;
+  abbreviation: string;
+  'short-title-zh': string;
+  'title-zh': string;
+  volumes: number;
+}
+
+export interface MenuProps {
+  categories: Record<string, Category>;
+}
+
+export default function Menu({ categories = {} }: MenuProps) {
   return <>
-    <h2>orihon memu</h2>
+    <h2>册别目录</h2>
+    <div>
+      <nav>
+        {Object.keys(categories).map(key => {
+          const category = categories[key];
+          const { title, abbreviation } = category;
+
+          return (
+            <li key={title}>
+              <Link href={`/category/${key}`} passHref>
+                <span>
+                  <em>{key}</em> {category['title-zh'] || title}
+                </span>
+              </Link>
+            </li>
+          );
+        })}
+      </nav>
+    </div>
   </>
+}
+
+export async function getStaticProps() {
+  const result = await fetch('https://orihon.vercel.app/api/category');
+  const categories = await result.json();
+  
+  return {
+    props: {
+      categories,
+    },
+  };
 }
