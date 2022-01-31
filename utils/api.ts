8124821508d3ebdf914242ce144f66da2cbc3a16
陆沉 <surgesoft@gmail.com>
2022-01-31 18:20:59 +0800

    feat: wtf..

--- utils/api.ts
+++ utils/api.ts
@@ -2,14 +2,14 @@ import type { Category, SubCategory } from "../pages/api/category";
 
 
 export async function getCategoryList(): Promise<Record<string, Category>> {
-  const endpoint = process.env.VERCEL_URL || 'orihon.vercel.app';
+  const endpoint = process.env.NEXT_PUBLIC_VERCEL_URL || 'orihon.vercel.app';
   const result = await fetch(`https://${endpoint}/api/category`);
   return await result.json();
 }
 
 
 export async function getSubCategoryById(id: string): Promise<SubCategory[]> {
-  const endpoint = process.env.VERCEL_URL || 'orihon.vercel.app';
+  const endpoint = process.env.NEXT_PUBLIC_VERCEL_URL || 'orihon.vercel.app';
   const result = await fetch(`https://${endpoint}/api/category/${id}`);
   // 万一没有
   if (result.status === 404) {
