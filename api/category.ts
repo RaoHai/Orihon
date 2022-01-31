be60b90526f98a896fd2cf26d96269daf007fe1d
陆沉 <surgesoft@gmail.com>
2022-01-31 18:42:14 +0800

    feat: cherry-pick api

--- api/category.ts
+++
@@ -1,8 +0,0 @@
-import urllib from 'urllib';
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default async function handler(_req: VercelRequest, res: VercelResponse) {
-  res.setHeader('Cache-Control', 's-maxage=86400');
-  const result = await urllib.curl('https://raw.githubusercontent.com/cbeta-org/xml-p5/master/canons.json', { dataType: 'json' });
-  res.status(200).json(result.data);
-}
